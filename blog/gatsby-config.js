424d4b6f705dbf224b6c7f00382eca65418c79ee
Renad Abdelhay <abdelhar@gmatc.matc.edu>
2024-02-01 19:43:17 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: ` `,
+    author: `Renad Abdelhay`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Renad Abdelhay`,
+      company: `Blogs Inc`,
+      address: `PO Box 1235`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
